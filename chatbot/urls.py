135336a96e168814481d98ef69c9868e383290c1
med <medmiskini@gmail.com>
2018-04-10 18:57:53 +0100

    10 avril

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -5,7 +5,8 @@ from django.urls import path
 
 from . import views
 
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 app_name = 'chatbot'
@@ -15,3 +16,6 @@ urlpatterns = [
     path('cvmodule/',views.cvmodule, name='cvmodule'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
